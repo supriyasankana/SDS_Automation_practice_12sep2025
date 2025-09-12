a15aec239cb6329ba6f3a8a46070ae2fcb2ba356
DHIRAJ <DHIRAJ@DESKTOP-BSCQ4A7>
2025-09-13 00:02:31 +0530

    Adding project to github repository

---
+++ TestMavenProj_2025/src/test/java/javapackage/Test1.java
@@ -0,0 +1,20 @@
+package javapackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	@Test
+	public void a() {
+		
+WebDriverManager.chromedriver().setup();
+WebDriver driver=new ChromeDriver();
+driver.get("https://www.facebook.com/");
+System.out.println("hello Git");
+	}
+
+}
